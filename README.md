a1f6df149a4bc3a59b174a17d3331f5cb0c39251
Swastik2442 <kul.swas321tik@gmail.com>
2021-08-23 19:42:27 +0530

    Design Changes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Chat-App
- Real Time Chat Application using Node.js & Socket.io
+# ChatApp
+Real-Time Chat Application using [Node.js](https://nodejs.org/) & [Socket.io](https://socket.io/)
+
+## Working
+* Clone the Repository to your Host.
+* Deploy the Chat Server using ["./nodeServer/index.js"](https://github.com/Swastik2442/ChatApp/blob/main/nodeServer/index.js).
+* Deploy the Chat Service using ["./index.js"](https://github.com/Swastik2442/ChatApp/blob/main/index.html).
+
+<hr>
+<div align = "center">
+
+Made with ❤ by [Swastik](https://github.com/Swastik2442/)
+<!-- Watch Tutorial Video by CodeWithHarry ~ https://youtu.be/3QNBVG2yqKA -->
+
+</div>
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,21 +1,21 @@
-// Node server which will handle socket io connections
+// Node server which will handle Socket.io connections
 const io = require('socket.io')(8000)
 
 const users = {};
 
 io.on('connection', socket =>{
-    // If any new user joins, let other users connected to the server know!
+    // If any new user joins, broadcast it to others
     socket.on('new-user-joined', name =>{ 
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
-    // If someone sends a message, broadcast it to other people
+    // If someone sends a message, broadcast it to others
     socket.on('send', message =>{
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     });
 
-    // If someone leaves the chat, let others know 
+    // If someone leaves the chat, broadcast it to others
     socket.on('disconnect', message =>{
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
